e9f8525f305540011092f56fe0a8cdd7302689f3
Najiba Chaoua <najiba@littlebigsmala.com>
2023-01-31 20:26:15 +0100

    fixes

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ app.use("/files", Files);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 // });
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req,res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(5000, () => {
   console.log("Server started on 5000");
 });
