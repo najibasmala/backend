89bcf2289a1040456ff0d1ef96a57a869015a85d
Najiba Chaoua <najiba@littlebigsmala.com>
2023-02-21 09:01:53 +0100

    fix

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen("https://elearning-w-api.onrender.com", () => {
+app.listen(5000, () => {
   console.log("Server started on 5000");
 });
